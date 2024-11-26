700f1da30849a9f0cf0d3b38f1e6d4b186ba3ff1
seed <yellyhaze000@gmail.com>
2024-11-26 23:38:32 +0800

    v7

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*'); 
 
-Route::get('/storage/{path}', function ($path) {
-    return response()->file(storage_path("/storage/{$path}"));
-})->where('path', '.*');
+// Route::get('/storage/{path}', function ($path) {
+//     return response()->file(storage_path("/storage/{$path}"));
+// })->where('path', '.*');
