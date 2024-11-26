bed8fcf56a8aa0c5a1e4b81a01a9b8b9550c8696
seed <yellyhaze000@gmail.com>
2024-11-26 22:24:17 +0800

    v2

--- resources/js/components/views/axios-client.js
+++ resources/js/components/views/axios-client.js
@@ -4,6 +4,12 @@ const axiosService = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
+
+const csrfTokenElement = document.querySelector('meta[name="csrf-token"]');
+if (csrfTokenElement) {
+    axiosService.defaults.headers.common['X-CSRF-TOKEN'] = csrfTokenElement.getAttribute('content');
+}
+
 axiosService.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     config.headers.Authorization = `Bearer ${token}`;
